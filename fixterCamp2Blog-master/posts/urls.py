7b5a63df3a93715b55e61fadb17fa9b42d19433f
Carlos <carlos@la-MacBook-Air-de-Carlos.local>
2016-10-20 11:00:18 -0500

    proyecto inicio

---
+++ fixterCamp2Blog-master/posts/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from . import views
+
+
+urlpatterns = [
+	url(r'^$',views.Hola.as_view(),name="home"),
+	url(r'^blog/$',views.Listado.as_view(),name="lista"),
+	url(r'^nuevo/$',views.Nuevo.as_view(),name="nuevo"),
+]
